16d778b4947c78020adbe4d8761ddec60d40269b
ctes930013 <a0984108310@gmail.com>
2022-03-12 13:36:03 +0800

    refactor:更改模式

--- lib/web.dart
+++ lib/web.dart
@@ -21,10 +21,13 @@ class Web extends StatefulWidget{
 
 class _WebState extends State<Web> {
 
+  TextEditingController keyText = TextEditingController();      //欄位輸入框
   //定義webview的url
   final String url = "lib/assets/flutter.html";
   late String txt;
   late InAppWebViewController _controller;
+  //紀錄原本的web content
+  String originWebContent = "";
   //紀錄重組過後的web content
   String newWebContent = "";
   @override
@@ -37,74 +40,69 @@ class _WebState extends State<Web> {
         title: const Text("WebView"),
       ),
       body:
-      FutureBuilder<String>(
-        future: loadHtml(),
-        builder: (context, snap){
-          if(!snap.hasData){
-            return Container();
-          }
-          //取得html
-          newWebContent = snap.data ?? "";
-
-          resetWebContent();
-
-          return InAppWebView(
-            initialData: InAppWebViewInitialData(data: newWebContent),
-            initialOptions: InAppWebViewGroupOptions(
-                android: AndroidInAppWebViewOptions(
-                useHybridComposition: true    //解決部分android裝置不能輸入中文問題
-              )
+        Column(
+          children: [
+            //app輸入框
+            Container(
+              margin: const EdgeInsets.all(15),
+              child: Row(
+                mainAxisSize: MainAxisSize.min,
+                children: [
+                  SizedBox(
+                    width: 250,
+                    child: TextField(
+                      controller: keyText,
+                      keyboardType: TextInputType.text,
+                      decoration: const InputDecoration(
+                        icon: Icon(Icons.title),
+                        labelText: "請輸入要傳入WebView的值",
+                      ),
+                    ),
+                  ),
+                  Expanded(
+                    child: MaterialButton(
+                        child: const Text(
+                            '送出',
+                            style: TextStyle(
+                                fontWeight: FontWeight.bold,
+                                color: Colors.white
+                            )
+                        ),
+                        color: Colors.blueAccent,
+                        onPressed: () => {
+                          submit(context)
+                        }
+                    ),
+                  ),
+                ],
+              ),
             ),
-            onWebViewCreated: (controller) {
-              _controller = controller
-                // ..addJavaScriptHandler(      //傳值給web端
-                //   handlerName: 'fromFlutter',
-                //   callback: (data) {
-                //     return txt;    //send data to web
-                //   },
-                // )
-                ..addJavaScriptHandler(      //監聽web端傳入的值
-                  handlerName: 'toFlutter',
-                  callback: (data) {
-                    //show web message
-                    ScaffoldMessenger.of(context).showSnackBar(SnackBar(
-                      content: Text("Web端回傳的值: ${data[0]}"),
-                    ));    //get data from web
+            //webview
+            Expanded(
+                child: InAppWebView(
+                  initialFile: url,
+                  //initialData: InAppWebViewInitialData(data: newWebContent),
+                  initialOptions: webOptions(),
+                  onWebViewCreated: (controller) {
+                    webController(controller);
                   },
-                )
-                ..addJavaScriptHandler(      //監聽web端傳入的換頁
-                  handlerName: 'toFlutterPage',
-                  callback: (data) {
-                    //檢查點擊事件連結的開頭
-                    String urlScheme = data[0];
-                    if(urlScheme.startsWith("flutterweb2://")) {
-                      String scheme = urlScheme.split("://")[0] ?? '';
-                      String host = urlScheme.split("://")[1] ?? '';
-                      if(host.startsWith("result")){
-                        //移除該頁面
-                        //Navigator.pop(context);
-                        //換頁
-                        Navigator.push(context, MaterialPageRoute(builder: (context) => const Result(
-                          value: "這是原生App",
-                        )));
-                        //webview不要換頁
-                      }
-                    }  //get data from web
+                  onLoadStart: (controller, url) {
+                    //避免重複加載
+                    if(originWebContent == ""){
+                      resetWebContent(txt);
+                    }
                   },
-                );
-            },
-            onLoadStart: (controller, url) {
-
-            },
-          );
-        },),
-      floatingActionButton: FloatingActionButton(
-        onPressed: (){
-          showWebCotent();
-        },
-        tooltip: '內文',
-        child: const Text('內文'),
-      ),
+                ),
+            ),
+          ],
+        ),
+        floatingActionButton: FloatingActionButton(
+          onPressed: (){
+            showWebCotent();
+          },
+          tooltip: '內文',
+          child: const Text('內文'),
+        ),
     );
   }
 
@@ -120,13 +118,78 @@ class _WebState extends State<Web> {
   }
 
   //重組內文
-  resetWebContent(){
-    //替換成使用者於上一頁輸入的字符
-    newWebContent += "<h4>"+txt+"</h4>";
+  resetWebContent(String txt){
+    _controller.evaluateJavascript(source: htmlShowStr).then((result) {
+      if(originWebContent == ""){
+        //get web content
+        originWebContent = result;
+      }
+      //加上使用者輸入的字符
+      newWebContent = originWebContent + "<h4>從原生App傳入的值: "+txt+"</h4>";
+      _controller.loadData(data: newWebContent);
+    });
   }
 
-  //載入html檔案
-  loadHtml(){
-    return rootBundle.loadString(url);
+  //webview的設定
+  InAppWebViewGroupOptions webOptions(){
+    return InAppWebViewGroupOptions(
+        android: AndroidInAppWebViewOptions(
+            useHybridComposition: true    //解決部分android裝置不能輸入中文問題
+        )
+    );
+  }
+
+  //webview的控制器
+  InAppWebViewController webController(controller){
+    _controller = controller
+    // ..addJavaScriptHandler(      //傳值給web端
+    //   handlerName: 'fromFlutter',
+    //   callback: (data) {
+    //     return txt;    //send data to web
+    //   },
+    // )
+      ..addJavaScriptHandler(      //監聽web端傳入的值
+        handlerName: 'toFlutter',
+        callback: (data) {
+          //show web message
+          ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+            content: Text("Web端回傳的值: ${data[0]}"),
+          ));    //get data from web
+        },
+      )
+      ..addJavaScriptHandler(      //監聽web端傳入的換頁
+        handlerName: 'toFlutterPage',
+        callback: (data) {
+          //檢查點擊事件連結的開頭
+          String urlScheme = data[0];
+          if(urlScheme.startsWith("flutterweb2://")) {
+            String scheme = urlScheme.split("://")[0] ?? '';
+            String host = urlScheme.split("://")[1] ?? '';
+            if(host.startsWith("result")){
+              //移除該頁面
+              //Navigator.pop(context);
+              //換頁
+              Navigator.push(context, MaterialPageRoute(builder: (context) => const Result(
+                value: "這是原生App",
+              )));
+              //webview不要換頁
+            }
+          }  //get data from web
+        },
+      );
+    return _controller;
+  }
+
+  //傳值到webview按鈕觸發事件
+  void submit(context) {
+    String txt = keyText.value.text;
+    if(txt == ""){
+      ScaffoldMessenger.of(context).showSnackBar(const SnackBar(
+        content: Text("請勿為空"),
+      ));
+      return;
+    }
+
+    resetWebContent(txt);
   }
 }
